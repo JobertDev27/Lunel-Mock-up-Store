385311c988207a290013cb7ee505ea3cf7d5b150
JobertDev27 <jobertdev27@gmail.com>
2025-04-19 18:07:33 +0800

    created banner component

---
+++ src/components/CategoryBanner/CategoryBanner.jsx
@@ -0,0 +1,13 @@
+import { Link } from "react-router-dom";
+
+export default function CategoryBanner({ bgImage, categoryLabel }) {
+  return (
+    <div className="category-banner">
+      <img src={bgImage} alt="banner image" />
+      <div className="banner-content">
+        <p>{categoryLabel}</p>
+        <Link to="/shop">SHOP NOW</Link>
+      </div>
+    </div>
+  );
+}
