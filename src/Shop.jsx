70b5da39735fb3aed81e14e70f2a7da5a1129318
JobertDev27 <jobertdev27@gmail.com>
2025-04-23 21:28:26 +0800

    designed shop page, added items and hover animation

--- src/Shop.jsx
+++ src/Shop.jsx
@@ -1,3 +1,32 @@
+import Header from "./components/Header/Header";
+import useFetch from "./components/UseFetch/useFetch";
+import ItemContainer from "./components/ItemContainer/ItemContainer";
+import "./style/main.css";
+
 export default function Shop() {
-  return <h1>welcome to the shop page</h1>;
+  const dress = useFetch("womens-dresses");
+  const cosmetics = useFetch("beauty");
+  const jewelry = useFetch("womens-jewellery");
+  const bag = useFetch("womens-bags");
+  const shoes = useFetch("womens-shoes");
+  const watch = useFetch("womens-watches");
+  const fragrance = useFetch("fragrances");
+
+  console.log(dress);
+  return (
+    <>
+      <Header />
+      <main>
+        <div className="items-list">
+          {dress?.map((item) => (
+            <ItemContainer
+              image={item?.thumbnail}
+              title={item?.title}
+              price={item?.price}
+            />
+          ))}
+        </div>
+      </main>
+    </>
+  );
 }
