8e41f11eff1b5b267f3959bfb56d4003cc60e9ac
JobertDev27 <jobertdev27@gmail.com>
2025-04-25 23:22:22 +0800

    made footer a component

--- src/Home.jsx
+++ src/Home.jsx
@@ -17,6 +17,7 @@ import catBg1 from "./assets/clothing-category-bg.webp";
 import catBg2 from "./assets/accessory-category-bg.webp";
 import catBg3 from "./assets/cosmetics-category-bg.webp";
 import pfp from "./assets/user-image.webp";
+import Footer from "./components/Footer";
 
 export default function Home() {
   return (
@@ -183,31 +184,7 @@ export default function Home() {
             <b>— Isabelle M.</b>
           </div>
         </motion.section>
-        <footer>
-          <div>
-            <b>Menu</b>
-            <p>Home</p>
-            <p>Shop</p>
-            <p>Contact us</p>
-          </div>
-          <div>
-            <b>Categories</b>
-            <p>Clothing</p>
-            <p>Accessiories</p>
-            <p>cosmetics</p>
-            <p>Jewelry</p>
-          </div>
-          <div>
-            <b>Social Media</b>
-            <p>Twitter</p>
-            <p>Facebook</p>
-            <p>Instagram</p>
-            <p>Pinterest</p>
-          </div>
-          <div id="copyright-footer">
-            <p>Copyright &copy;2025 Designed by: Jobertdev27</p>
-          </div>
-        </footer>
+        <Footer />
       </main>
     </>
   );
---
+++ src/components/Footer.jsx
@@ -0,0 +1,29 @@
+export default function Footer() {
+  return (
+    <footer>
+      <div>
+        <b>Menu</b>
+        <p>Home</p>
+        <p>Shop</p>
+        <p>Contact us</p>
+      </div>
+      <div>
+        <b>Categories</b>
+        <p>Clothing</p>
+        <p>Accessiories</p>
+        <p>cosmetics</p>
+        <p>Jewelry</p>
+      </div>
+      <div>
+        <b>Social Media</b>
+        <p>Twitter</p>
+        <p>Facebook</p>
+        <p>Instagram</p>
+        <p>Pinterest</p>
+      </div>
+      <div id="copyright-footer">
+        <p>Copyright &copy;2025 Designed by: Jobertdev27</p>
+      </div>
+    </footer>
+  );
+}
